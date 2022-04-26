95bdb1fcdec3f3153d2a4454a5df54433e84ba57
KeLong <kelong23042001@gmail.com>
2022-04-26 07:33:43 +0700

    bai tap diem danh

---
+++ BaiTapDiemDanhSo2/PTBac2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapDiemDanhSo2
+{
+    class PTBac2
+    {
+    }
+}
---
+++ BaiTapDiemDanhSo2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BaiTapDiemDanhSo2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
