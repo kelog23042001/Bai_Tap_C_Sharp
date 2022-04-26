ea9abbf96d7d018c2623d33163bc86a5c60f3b3e
KeLong <kelong23042001@gmail.com>
2022-04-26 08:10:28 +0700

    Bai tap diem danh so 2

--- BaiTapDiemDanhSo2_1/Animal.cs
+++ BaiTapDiemDanhSo2_1/Animal.cs
@@ -4,15 +4,51 @@ using System.Text;
 
 namespace BaiTapDiemDanhSo2_1
 {
-    class Animal
+    public abstract class  Animal
     {
-        public virtual void eat()
+        protected Animal()
         {
-
         }
-        public virtual void makeSound()
-        {
 
+
+        public abstract void eat();
+        public abstract void makeSound();
+
+       
+    }
+
+    public class Cat : Animal
+    {
+    
+
+        public override void eat()
+        {
+            Console.WriteLine("Con meo an thit ca");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Meow meow meow");
+        }
+        public void run()
+        {
+            Console.WriteLine("Con meo chay thay nhanh");
         }
     }
+    public class Bird : Animal
+    {     
+
+        public override void eat()
+        {
+            Console.WriteLine("Con chim an banh mi");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Chip Chip Chip");
+        }
+        public void fly()
+        {
+            Console.WriteLine("Con chim bay toc do");
+        }
+    }
+
 }
--- BaiTapDiemDanhSo2_1/Program.cs
+++ BaiTapDiemDanhSo2_1/Program.cs
@@ -6,7 +6,15 @@ namespace BaiTapDiemDanhSo2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cat cat_232 = new Cat();
+            Bird bird_232 = new Bird();
+
+            cat_232.eat();
+            cat_232.makeSound();
+            cat_232.run();
+            bird_232.eat();
+            bird_232.makeSound();
+            bird_232.fly();
         }
     }
 }
