1c4143d4d671e86faeb32fe4af57864c0e164582
KeLong <kelong23042001@gmail.com>
2022-05-10 07:53:54 +0700

    min_max

--- BaiTapDiemDanhSo3/Program.cs
+++ BaiTapDiemDanhSo3/Program.cs
@@ -39,16 +39,43 @@ namespace BaiTapDiemDanhSo3
         }
         public static void sort_up(int[] arr_232)
         {
+            Console.WriteLine("Sap xep tang dan: ");
             int temp = 0;
             for (int i = 0; i < 10; i++)
             {
-               if(arr_232[i] < arr_232[i + 1])
+                for (int j = i + 1; j < 10; j++)
                 {
-                    arr_232[i] = temp;
-                    temp = arr_232[i + 1];
-                    arr_232[i + 1] = arr_232[i];
+                    if (arr_232[i] > arr_232[j])
+                    {
+                        temp = arr_232[i];
+                        arr_232[i] = arr_232[j];
+                        arr_232[j] = temp;
+                    }
                 }
+               
             }
+            Console.WriteLine("Mang vua sap xep: ");
+            output(arr_232);
+        }
+        public static void sort_dow(int[] arr_232)
+        {
+            Console.WriteLine("Sap xep giam dan: ");
+            int temp = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (arr_232[i] < arr_232[j])
+                    {
+                        temp = arr_232[i];
+                        arr_232[i] = arr_232[j];
+                        arr_232[j] = temp;
+                    }
+                }
+
+            }
+            Console.WriteLine("Mang vua sap xep: ");
+            output(arr_232);
         }
         static void Main(string[] args)
         {
@@ -56,6 +83,8 @@ namespace BaiTapDiemDanhSo3
             input(arr_232);
             output(arr_232);
             max_min(arr_232);
+            sort_up(arr_232);
+            sort_dow(arr_232);
         }
     }
 }
