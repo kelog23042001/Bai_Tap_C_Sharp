e2cbdf1c9e842091ee18b3beffe5932f6d78b510
KeLong <kelong23042001@gmail.com>
2022-05-10 07:41:11 +0700

    them duoi 232

--- BaiTapDiemDanhSo3/Program.cs
+++ BaiTapDiemDanhSo3/Program.cs
@@ -4,45 +4,58 @@ namespace BaiTapDiemDanhSo3
 {
     class Program
     {
-        public static void input(int[] arr)
+        public static void input(int[] arr_232)
         {
             
             Console.WriteLine("Nhap mang: ");
-            for (int i = 0; i < 10; i++)
+            for (int i_232 = 0; i_232 < 10; i_232++)
             {
-                Console.Write("Nhap arr[{0}] =", i);
-                arr[i] = Int32.Parse(Console.ReadLine());
+                Console.Write("Nhap arr_232[{0}] =", i_232);
+                arr_232[i_232] = Int32.Parse(Console.ReadLine());
             }
         }
 
-        public static void output(int[] arr)
+        public static void output(int[] arr_232)
         {
             Console.WriteLine("Mang da nhap: ");
-            for (int i = 0; i < 10; i++)
+            for (int i_232 = 0; i_232 < 10; i_232++)
             {
-                Console.WriteLine("a[{0}] = {1}", i, arr[i]);
+                Console.WriteLine("a[{0}] = {1}", i_232, arr_232[i_232]);
             }
         }
-        public static void max_min(int[] arr)
+        public static void max_min(int[] arr_232)
         {
-            int max = arr[0], min = arr[0];
+            int max = arr_232[0], min = arr_232[0];
             for (int i = 1; i < 10; i++)
             {
-                if (max < arr[i])
-                    max = arr[i];
-                if (min > arr[i])
-                    min = arr[i];
+                if (max < arr_232[i])
+                    max = arr_232[i];
+                if (min > arr_232[i])
+                    min = arr_232[i];
             }
             Console.WriteLine("Gia tri lon nhat: {0}", max);
             Console.WriteLine("Gia tri nho nhat: {0}", min);
 
         }
+        public static void sort_up(int[] arr_232)
+        {
+            int temp = 0;
+            for (int i = 0; i < 10; i++)
+            {
+               if(arr_232[i] < arr_232[i + 1])
+                {
+                    arr_232[i] = temp;
+                    temp = arr_232[i + 1];
+                    arr_232[i + 1] = arr_232[i];
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
-            input(arr);
-            output(arr);
-            max_min(arr);
+            int[] arr_232 = new int[10];
+            input(arr_232);
+            output(arr_232);
+            max_min(arr_232);
         }
     }
 }
